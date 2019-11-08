661eebe76e01865f5bacda34ea6bc7d7677c4718
Cristian SC <sifuentescovarrubiasitsl@gmail.com>
2019-11-07 23:25:05 -0600

    CSC 20191107: Examen

--- app.js
+++ app.js
@@ -122,18 +122,68 @@
 // };
 // enviarMision(wolverine);
 // enviarCuartel(wolverine);
-var Avenger = /** @class */ (function () {
-    //funcion que se crea cuando se crea una nueva instancia de este objeto
-    function Avenger(nombre, equipo, nombreReal) {
-        this.nombre = 'sin nombre';
-        this.equipo = '';
-        this.nombreReal = '';
-        this.peleasGanadas = 0;
-        this.nombre = nombre;
-        this.equipo = equipo;
-        this.nombreReal = nombreReal;
+// class Avenger {
+// 	nombre: string = 'sin nombre';
+// 	equipo: string = '';
+// 	nombreReal: string = '';
+// 	puedePelear: boolean | undefined;
+// 	peleasGanadas: number = 0;
+// 	//funcion que se crea cuando se crea una nueva instancia de este objeto
+// 	constructor(nombre: string, equipo: string, nombreReal: string) {
+// 		this.nombre = nombre;
+// 		this.equipo = equipo;
+// 		this.nombreReal = nombreReal;
+// 	}
+// }
+// let antman: Avenger = new Avenger('Antman', 'Cap', 'Scott lang');
+// console.log(antman);
+/*Examen*/
+// Uso de Let y Const
+var nombre = 'Ricardo Tapia';
+var edad = 23;
+var PERSONAJE = {
+    nombre: nombre,
+    edad: edad
+};
+var batman = {
+    nombre: 'Bruno Díaz',
+    artesMarciales: ['Karate', 'Aikido', 'Wing Chun', 'Jiu-Jitsu']
+};
+// Convertir esta funcion a una funcion de flecha
+function resultadoDoble(a, b) {
+    return (a + b) * 2;
+}
+var resultadoDobleF = function (a, b) { return (a + b) * 2; };
+// Función con parametros obligatorios, opcionales y por defecto
+// donde NOMBRE = obligatorio
+//       PODER  = opcional
+//       ARMA   = por defecto = "arco"
+function getAvenger(nombre, arma, poder) {
+    if (arma === void 0) { arma = 'arco'; }
+    var mensaje;
+    if (poder) {
+        mensaje = nombre + ' tiene el poder de: ' + poder + ' y un arma: ' + arma;
     }
-    return Avenger;
+    else {
+        mensaje = nombre + ' tiene un ' + poder;
+    }
+}
+// Cree una clase que permita manejar la siguiente estructura
+// La clase se debe de llamar rectangulo,
+// debe de tener dos propiedades:
+//   * base
+//   * altura
+// También un método que calcule el área  =  base * altura,
+// ese método debe de retornar un numero.
+var Rectangulo = /** @class */ (function () {
+    //funcion que se crea cuando se crea una nueva instancia de este objeto
+    function Rectangulo(base, altura) {
+        this.base = 0;
+        this.altura = 0;
+        this.resultado = 0.0;
+        this.resultado = base * altura;
+    }
+    return Rectangulo;
 }());
-var antman = new Avenger('Antman', 'Cap', 'Scott lang');
-console.log(antman);
+var valor = new Rectangulo(1, 9);
+console.log(valor.resultado);
--- app.ts
+++ app.ts
@@ -165,19 +165,80 @@
 // enviarMision(wolverine);
 // enviarCuartel(wolverine);
 
-class Avenger {
-	nombre: string = 'sin nombre';
-	equipo: string = '';
-	nombreReal: string = '';
-	puedePelear: boolean | undefined;
-	peleasGanadas: number = 0;
-	//funcion que se crea cuando se crea una nueva instancia de este objeto
-	constructor(nombre: string, equipo: string, nombreReal: string) {
-		this.nombre = nombre;
-		this.equipo = equipo;
-		this.nombreReal = nombreReal;
+// class Avenger {
+// 	nombre: string = 'sin nombre';
+// 	equipo: string = '';
+// 	nombreReal: string = '';
+// 	puedePelear: boolean | undefined;
+// 	peleasGanadas: number = 0;
+// 	//funcion que se crea cuando se crea una nueva instancia de este objeto
+// 	constructor(nombre: string, equipo: string, nombreReal: string) {
+// 		this.nombre = nombre;
+// 		this.equipo = equipo;
+// 		this.nombreReal = nombreReal;
+// 	}
+// }
+
+// let antman: Avenger = new Avenger('Antman', 'Cap', 'Scott lang');
+// console.log(antman);
+
+/*Examen*/
+// Uso de Let y Const
+var nombre = 'Ricardo Tapia';
+var edad = 23;
+var PERSONAJE = {
+	nombre: nombre,
+	edad: edad
+};
+
+// Cree una interfaz que sirva para validar el siguiente objeto
+interface Batman {
+	nombre: string;
+	artesMarciales: Array<string>;
+}
+
+let batman: Batman = {
+	nombre: 'Bruno Díaz',
+	artesMarciales: [ 'Karate', 'Aikido', 'Wing Chun', 'Jiu-Jitsu' ]
+};
+
+// Convertir esta funcion a una funcion de flecha
+function resultadoDoble(a: number, b: number) {
+	return (a + b) * 2;
+}
+
+let resultadoDobleF = (a: number, b: number) => (a + b) * 2;
+
+// Función con parametros obligatorios, opcionales y por defecto
+// donde NOMBRE = obligatorio
+//       PODER  = opcional
+//       ARMA   = por defecto = "arco"
+function getAvenger(nombre: string, arma: string = 'arco', poder?: string) {
+	var mensaje;
+	if (poder) {
+		mensaje = nombre + ' tiene el poder de: ' + poder + ' y un arma: ' + arma;
+	} else {
+		mensaje = nombre + ' tiene un ' + poder;
 	}
 }
 
-let antman: Avenger = new Avenger('Antman', 'Cap', 'Scott lang');
-console.log(antman);
+// Cree una clase que permita manejar la siguiente estructura
+// La clase se debe de llamar rectangulo,
+// debe de tener dos propiedades:
+//   * base
+//   * altura
+// También un método que calcule el área  =  base * altura,
+// ese método debe de retornar un numero.
+
+class Rectangulo {
+	base: number = 0;
+	altura: number = 0;
+	resultado: number = 0.0;
+	//funcion que se crea cuando se crea una nueva instancia de este objeto
+	constructor(base: number, altura: number) {
+		this.resultado = base * altura;
+	}
+}
+
+let valor: Rectangulo = new Rectangulo(1, 9);
+console.log(valor.resultado);
