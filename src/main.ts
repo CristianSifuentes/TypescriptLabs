a33b4f47b87ef6c97aa95dd510813981fb4daef3
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2025-03-26 11:30:30 -0600

    modify folder name, add code for basic types and object interface in TypeScript

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import './style.css'
+//import './topics/01-basic-types';
+// module required, encapsulated grouping of a procedure
+import './topics/02-object-interface';
+
 
 const app = document.querySelector<HTMLDivElement>('#app');
 
